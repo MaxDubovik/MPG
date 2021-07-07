e1207b747b32c783434dc7503ee1a91586e3d197
Max <maks.dubovik.98@gmail.com>
2021-07-07 22:58:07 +0300

    add animation

--- js/script.js
+++ js/script.js
@@ -27,7 +27,44 @@ window.addEventListener('DOMContentLoaded', () => {
             hamburger.classList.toggle('hamburger_active');
             menu.classList.toggle('nav__menu_active');
         })
-    })
+    }) 
+
+    const animItems = document.querySelectorAll('._anim-items');  
+
+    if (animItems.length > 0) {
+        window.addEventListener('scroll', animOnScroll);
+        function animOnScroll() {
+            for (let index = 0; index < animItems.length; index++) {
+                const animItem = animItems[index];
+                const animItemHeight = animItem.offsetHeight;
+                const animItemOffset = offset(animItem).top;
+                const animStart = 8;
+
+                let animItemPoint = window.innerHeight - animItemHeight / animStart;
+                if (animItemHeight > window.innerHeight) {
+                    animItemPoint = window.innerHeight - window.innerHeight / animStart;
+                }
+
+                if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                    animItem.classList.add('_active');
+                } else {
+                    if (!animItem.classList.contains('_anim-no-hide')) {
+                        animItem.classList.remove('_active');
+                    }                  
+                }
+            }
+        }
+        function offset(el) {
+            const rect = el.getBoundingClientRect(),
+                scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+                scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+        }
+        
+        setTimeout(() => {
+            animOnScroll();
+        }, 300);    
+    }
 })
 
 // $(document).ready(function(){
