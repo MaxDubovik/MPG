66edb283fef861946e47a90126bd2f4c0ff6eab0
Max <maks.dubovik.98@gmail.com>
2021-07-10 19:46:13 +0300

    add valivate + send form

--- js/script.js
+++ js/script.js
@@ -14,26 +14,26 @@ window.addEventListener('DOMContentLoaded', () => {
         menu.classList.remove('nav__menu_active');
     });
 
-    done = document.querySelector('.footer__done');
-    form = document.querySelector('.footer__feedback');
-    submitBtn = document.querySelector('.feedback__button');
+    // done = document.querySelector('.footer__done');
+    // form = document.querySelector('.footer__feedback');
+    // submitBtn = document.querySelector('.feedback__button');
 
-    submitBtn.addEventListener('click', () => {
-        form.classList.remove('footer-active');
-        done.classList.add('footer-active');
-    });
+    // submitBtn.addEventListener('click', () => {
+    //     form.classList.remove('footer-active');
+    //     done.classList.add('footer-active');
+    // });
 
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
             menu.classList.remove('nav__menu_active');
         })
-    }) 
+    });
 
     menuLink.forEach(link => {
         link.addEventListener('click', () => {
             menu.classList.remove('nav__menu_active');
         })
-    }) 
+    }); 
 
     const animItems = document.querySelectorAll('._anim-items');  
 
@@ -59,17 +59,76 @@ window.addEventListener('DOMContentLoaded', () => {
                     }                  
                 }
             }
-        }
+        };
         function offset(el) {
             const rect = el.getBoundingClientRect(),
                 scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
                 scrollTop = window.pageYOffset || document.documentElement.scrollTop;
             return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-        }
+        };
         
         setTimeout(() => {
             animOnScroll();
         }, 300);    
+    };
+
+    const form = document.getElementById('feedback-form');
+    form.addEventListener('submit', formSend);
+
+    async function formSend(e) {
+        e.preventDefault();   
+        
+        let error = formValidate(form);
+
+        let formData = new FormData(form);
+
+        if (error === 0) {
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: formData
+            });
+            if (response.ok) {
+                let result = await response.json();
+                alert(result.message);
+                form.reset();
+            } else {
+                alert ('Server Error');
+            }
+        }
+    };
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            formRemoveError(input);
+
+            if (input.classList.contains('_email')) {
+                if(emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            } else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++;
+                }
+            }
+        }
+        return error;
+    }
+    function formAddError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
+    function emailTest(input) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
     }
 })
 
